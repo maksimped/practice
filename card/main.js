bb882fa3d9d2c5ba6472dc80210494b8972c82c6
Maksym <maksimlodz@gmaul.com>
2022-05-07 09:39:52 +0200

    anising

---
+++ card/main.js
@@ -0,0 +1,11 @@
+function t1() {
+    let a = document.querySelector('img')
+    a.src = 'https://images.ctfassets.net/uwf0n1j71a7j/4veOqyHMw35qOlTQqYt1BA/8be6b2de30cdafd3149d132f29c61e38/types-of-cars-in-india-car-body-types.png'
+    a.style.wdth = '100%'
+    console.log(a.src)
+
+
+}
+
+
+document.querySelector('.business').onclick = t1;
